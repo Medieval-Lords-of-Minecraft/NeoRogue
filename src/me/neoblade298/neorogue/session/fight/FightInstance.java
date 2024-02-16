d51645165662f331080244131b7db0a59d57c030
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2024-02-15 21:17:55 -0800

    make sure damage ticks is reset properly

--- src/me/neoblade298/neorogue/session/fight/FightInstance.java
+++ src/me/neoblade298/neorogue/session/fight/FightInstance.java
@@ -187,6 +187,7 @@ public abstract class FightInstance extends Instance {
 	public void handlePlayerLeave(Player p) {
 		p.setInvulnerable(false);
 		p.setInvisible(false);
+		p.setNoDamageTicks(10);
 		p.removePotionEffect(PotionEffectType.ABSORPTION);
 	}
 	
