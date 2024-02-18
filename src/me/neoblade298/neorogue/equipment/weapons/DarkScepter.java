5c0a8a571543f56c8a2da1b3f715b6b49669dfca
Alex <fopzl@proton.me>
2024-02-17 19:52:15 -0500

    New weps

--- src/me/neoblade298/neorogue/equipment/weapons/DarkScepter.java
+++ src/me/neoblade298/neorogue/equipment/weapons/DarkScepter.java
@@ -1,16 +1,11 @@
 package me.neoblade298.neorogue.equipment.weapons;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import me.neoblade298.neocore.bukkit.particles.ParticleContainer;
-import me.neoblade298.neocore.bukkit.util.Util;
-import me.neoblade298.neorogue.NeoRogue;
 import me.neoblade298.neorogue.equipment.Equipment;
 import me.neoblade298.neorogue.equipment.EquipmentProperties;
 import me.neoblade298.neorogue.equipment.EquipmentProperties.PropertyType;
@@ -22,54 +17,74 @@ import me.neoblade298.neorogue.equipment.mechanics.ProjectileInstance;
 import me.neoblade298.neorogue.session.fight.DamageType;
 import me.neoblade298.neorogue.session.fight.FightData;
 import me.neoblade298.neorogue.session.fight.PlayerFightData;
-import me.neoblade298.neorogue.session.fight.TargetHelper;
-import me.neoblade298.neorogue.session.fight.TargetHelper.TargetProperties;
-import me.neoblade298.neorogue.session.fight.TargetHelper.TargetType;
 import me.neoblade298.neorogue.session.fight.trigger.Trigger;
 import me.neoblade298.neorogue.session.fight.trigger.TriggerResult;
 
-public class FireStaff extends Equipment {
-	private static final TargetProperties props = TargetProperties.radius(1.2, true, TargetType.ENEMY);
-	
+public class DarkScepter extends Equipment {
 	private static ParticleContainer tick;
 
 	static {
-		tick = new ParticleContainer(Particle.FLAME);
+		tick = new ParticleContainer(Particle.GLOW);
 		tick.count(5).spread(0.1, 0.1).speed(0.01);
 	}
 
-	public FireStaff(boolean isUpgraded) {
+	public DarkScepter(boolean isUpgraded) {
 		super(
-				"fireStaff", "Fire Staff", isUpgraded, Rarity.COMMON, EquipmentClass.MAGE, EquipmentType.WEAPON,
-				EquipmentProperties.ofWeapon(8, 0, isUpgraded ? 40 : 20, 0.35, DamageType.FIRE, Sound.ITEM_AXE_SCRAPE)
+				"darkScepter", "Dark Scepter", isUpgraded, Rarity.COMMON, EquipmentClass.MAGE, EquipmentType.WEAPON,
+				EquipmentProperties.ofWeapon(6, 0, isUpgraded ? 50 : 25, 0.65, DamageType.DARK, Sound.ITEM_AXE_SCRAPE)
 		);
 		properties.addUpgrades(PropertyType.DAMAGE);
 	}
 	
 	@Override
 	public void initialize(Player p, PlayerFightData data, Trigger bind, EquipSlot es, int slot) {
-		ProjectileGroup proj = new ProjectileGroup(new FireStaffProjectile(p));
+		ProjectileGroup proj = new ProjectileGroup(new DarkScepterProjectile(p));
 		data.addSlotBasedTrigger(id, slot, Trigger.LEFT_CLICK, (d, inputs) -> {
 			if (!canUseWeapon(data))
 				return TriggerResult.keep();
 			weaponSwing(p, data);
-			data.addTask(id, new BukkitRunnable() {
-				@Override
-				public void run() {
-					proj.start(data);
-				}
-			}.runTaskLater(NeoRogue.inst(), 5));
+			proj.start(data);
 			return TriggerResult.keep();
 		});
 	}
 
-	private class FireStaffProjectile extends Projectile {
+	private class DarkScepterProjectile extends Projectile {
 		private Player p;
+		private boolean blocked = false;
 		
-		public FireStaffProjectile(Player p) {
-			super(0.5, 15, 2);
-			this.size(1, 1).gravity(0.125).initialY(0.55);
+		public DarkScepterProjectile(Player p) {
+			super(4, 15, 1);
+			this.size(0.5, 0.5).ignore(false, false, true);
 			this.p = p;
+		}
+		
+		@Override
+		public void onTick(ProjectileInstance proj, boolean interpolation) {
+		}
+		
+		@Override
+		public void onEnd(ProjectileInstance proj) {
+			if (blocked)
+				return;
+			
+			new ProjectileGroup(new DarkRayProjectile(p)).start(proj.getOwner());
+		}
+		
+		@Override
+		public void onHit(FightData hit, Barrier hitBarrier, ProjectileInstance proj) {
+			if (hitBarrier != null)
+				blocked = true;
+		}
+		
+		@Override
+		public void onStart(ProjectileInstance proj) {
+		}
+	}
+
+	private class DarkRayProjectile extends Projectile {
+		public DarkRayProjectile(Player p) {
+			super(0.5, 2.5, 1);
+			this.size(1.25, 1.25).pierce();
 		}
 		
 		@Override
@@ -79,27 +94,20 @@ public class FireStaff extends Equipment {
 		
 		@Override
 		public void onEnd(ProjectileInstance proj) {
-
 		}
 		
 		@Override
 		public void onHit(FightData hit, Barrier hitBarrier, ProjectileInstance proj) {
-			for (LivingEntity ent : TargetHelper.getEntitiesInRadius(p, hit.getEntity().getLocation(), props)) {
-				weaponDamageProjectile(ent, proj);
-			}
-
-			Location loc = hit.getEntity().getLocation();
-			Util.playSound(p, loc, Sound.BLOCK_CHAIN_PLACE, 1F, 1F, true);
+			weaponDamageProjectile(hit.getEntity(), proj, hitBarrier);
 		}
 		
 		@Override
 		public void onStart(ProjectileInstance proj) {
-
 		}
 	}
 	
 	@Override
 	public void setupItem() {
-		item = createItem(Material.STICK, "After a brief delay, lobs a small fireball exploding on hit.");
+		item = createItem(Material.STICK, "Dark rays shoot out of a surface hit.");
 	}
 }
---
+++ src/me/neoblade298/neorogue/equipment/weapons/EarthStaff.java
@@ -0,0 +1,98 @@
+package me.neoblade298.neorogue.equipment.weapons;
+
+import java.util.LinkedList;
+
+import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import me.neoblade298.neocore.bukkit.particles.Circle;
+import me.neoblade298.neocore.bukkit.particles.LocalAxes;
+import me.neoblade298.neocore.bukkit.particles.ParticleContainer;
+import me.neoblade298.neocore.bukkit.util.Util;
+import me.neoblade298.neorogue.NeoRogue;
+import me.neoblade298.neorogue.equipment.Equipment;
+import me.neoblade298.neorogue.equipment.EquipmentProperties;
+import me.neoblade298.neorogue.equipment.EquipmentProperties.PropertyType;
+import me.neoblade298.neorogue.equipment.Rarity;
+import me.neoblade298.neorogue.player.inventory.GlossaryTag;
+import me.neoblade298.neorogue.session.fight.DamageType;
+import me.neoblade298.neorogue.session.fight.FightInstance;
+import me.neoblade298.neorogue.session.fight.PlayerFightData;
+import me.neoblade298.neorogue.session.fight.TargetHelper;
+import me.neoblade298.neorogue.session.fight.TargetHelper.TargetProperties;
+import me.neoblade298.neorogue.session.fight.TargetHelper.TargetType;
+import me.neoblade298.neorogue.session.fight.status.Status.StatusType;
+import me.neoblade298.neorogue.session.fight.trigger.Trigger;
+import me.neoblade298.neorogue.session.fight.trigger.TriggerResult;
+
+public class EarthStaff extends Equipment {
+	// TODO: dont just hit in 3d radius, need to focus on enemies on floor
+
+	private static final double innerRadius = 1.5, outerRadius = 4;
+	private static final Circle innerRing = new Circle(innerRadius), outerRing = new Circle(outerRadius);
+	private static final ParticleContainer aoe = new ParticleContainer(Particle.GLOW).count(1).spread(0.1, 0.1).speed(0);
+
+	private static final TargetProperties innerProps = TargetProperties.radius(innerRadius, true, TargetType.ENEMY);
+	private static final TargetProperties outerProps = TargetProperties.radius(outerRadius, true, TargetType.ENEMY);
+
+	public EarthStaff(boolean isUpgraded) {
+		super(
+				"earthStaff", "Earth Staff", isUpgraded, Rarity.COMMON, EquipmentClass.MAGE, EquipmentType.WEAPON,
+				EquipmentProperties.ofWeapon(10, 0, isUpgraded ? 75 : 50, 0.25, DamageType.EARTHEN, Sound.ITEM_AXE_SCRAPE)
+		);
+		properties.addUpgrades(PropertyType.DAMAGE);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void initialize(Player p, PlayerFightData data, Trigger bind, EquipSlot es, int slot) {
+		data.addSlotBasedTrigger(id, slot, Trigger.LEFT_CLICK, (d, inputs) -> {
+			if (!canUseWeapon(data))
+				return TriggerResult.keep();
+			if (!p.isOnGround()) {
+				Util.displayError(p, "Can't use in the air!");
+			}
+			weaponSwing(p, data);
+			data.addTask(id, new BukkitRunnable() {
+				@Override
+				public void run() {
+					Util.playSound(p, Sound.ENTITY_GENERIC_EXPLODE, true);
+					innerRing.draw(aoe, p.getLocation(), LocalAxes.xz(), aoe);
+					LinkedList<LivingEntity> closeEnemies = TargetHelper.getEntitiesInRadius(p, innerProps);
+					for (LivingEntity ent : closeEnemies) {
+						weaponDamage(p, data, ent);
+						FightInstance.getFightData(ent.getUniqueId()).applyStatus(StatusType.CONCUSSED, p.getUniqueId(), 3, 0);
+					}
+					
+					data.addTask(id, new BukkitRunnable() {
+						@Override
+						public void run() {
+							Util.playSound(p, Sound.ENTITY_GENERIC_EXPLODE, true);
+							outerRing.draw(aoe, p.getLocation(), LocalAxes.xz(), aoe);
+							LinkedList<LivingEntity> farEnemies = TargetHelper.getEntitiesInRadius(p, outerProps);
+							farEnemies.removeAll(closeEnemies);
+							for (LivingEntity ent : farEnemies) {
+								weaponDamage(p, data, ent);
+								FightInstance.getFightData(ent.getUniqueId()).applyStatus(StatusType.CONCUSSED, p.getUniqueId(), 3, 0);
+							}
+						}
+					}.runTaskLater(NeoRogue.inst(), 3));
+				}
+			}.runTaskLater(NeoRogue.inst(), 12));
+			return TriggerResult.keep();
+		});
+	}
+	
+	@Override
+	public void setupItem() {
+		item = createItem(
+				Material.STICK,
+				"After a windup, smashes the ground beneath you, dealing damage in an area spreading outwards. Enemies closest to you receive <white>3</white> "
+						+ GlossaryTag.CONCUSSED.tag(this) + ". Must be cast while on the ground."
+		);
+	}
+}
