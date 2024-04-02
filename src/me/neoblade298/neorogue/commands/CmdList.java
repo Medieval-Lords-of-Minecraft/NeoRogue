58d0975606b5f37f2defa1f5094c86e7405b91e8
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2024-04-02 15:35:39 -0700

    Start adding cmdlist

---
+++ src/me/neoblade298/neorogue/commands/CmdList.java
@@ -0,0 +1,41 @@
+package me.neoblade298.neorogue.commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import me.neoblade298.neocore.bukkit.commands.Subcommand;
+import me.neoblade298.neocore.bukkit.util.Util;
+import me.neoblade298.neocore.shared.commands.SubcommandRunner;
+import me.neoblade298.neorogue.session.Instance;
+import me.neoblade298.neorogue.session.LobbyInstance;
+import me.neoblade298.neorogue.session.Session;
+import me.neoblade298.neorogue.session.SessionManager;
+
+public class CmdList extends Subcommand {
+
+	public CmdList(String key, String desc, String perm, SubcommandRunner runner) {
+		super(key, desc, perm, runner);
+		this.overrideTabHandler();
+		args.setOverride("{--type EquipmentType} {--rarity Rarity} {--class EquipmentClass} {--tags GlossaryTag1,Tag2...}"
+				+ " {--reforge Parent/Child/None}");
+		args.setMax(-1);
+	}
+
+	@Override
+	public void run(CommandSender s, String[] args) {
+		Player p = (Player) s;
+		Session sess = SessionManager.getSession(p);
+		if (sess == null) {
+			Util.displayError(p, "You're not in a session!");
+			return;
+		}
+		
+		Instance inst = sess.getInstance();
+		if (!(inst instanceof LobbyInstance)) {
+			Util.displayError(p, "You're not in a lobby!");
+			return;
+		}
+		
+		LobbyInstance li = (LobbyInstance) inst;
+		li.displayInfo(p);
+	}
+}
