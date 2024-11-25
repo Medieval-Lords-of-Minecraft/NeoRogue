4c3803ba2e8823f54723ea4285ca523ea3cb6029
Alex <fopzl@proton.me>
2024-11-25 17:57:26 -0500

    durr

--- src/me/neoblade298/neorogue/equipment/abilities/EndlessVenom.java
+++ src/me/neoblade298/neorogue/equipment/abilities/EndlessVenom.java
@@ -25,20 +25,23 @@ import me.neoblade298.neorogue.session.fight.trigger.event.BasicAttackEvent;
 
 public class EndlessVenom extends Equipment {
 	private static final String ID = "endlessVenom";
-	private int poison, dur;
-	private static final ParticleContainer pc = new ParticleContainer(Particle.DUST).dustOptions(new DustOptions(Color.GREEN, 1)).count(50).spread(0.5, 0.5).speed(0.2);
+	private int poison;
+	private static final ParticleContainer pc = new ParticleContainer(Particle.DUST)
+			.dustOptions(new DustOptions(Color.GREEN, 1)).count(50).spread(0.5, 0.5).speed(0.2);
 	private static final SoundContainer sc = new SoundContainer(Sound.ENTITY_GENERIC_SWIM);
-	
+
 	public EndlessVenom(boolean isUpgraded) {
-		super(ID, "Endless Venom", isUpgraded, Rarity.UNCOMMON, EquipmentClass.THIEF,
-				EquipmentType.ABILITY, EquipmentProperties.ofUsable(10, 10, 12, 0));
+		super(
+				ID, "Endless Venom", isUpgraded, Rarity.UNCOMMON, EquipmentClass.THIEF, EquipmentType.ABILITY,
+				EquipmentProperties.ofUsable(10, 10, 12, 0)
+		);
 		poison = isUpgraded ? 10 : 7;
 	}
-	
+
 	public static Equipment get() {
 		return Equipment.get(ID, false);
 	}
-
+	
 	@Override
 	public void initialize(Player p, PlayerFightData data, Trigger bind, EquipSlot es, int slot) {
 		StandardEquipmentInstance inst = new StandardEquipmentInstance(data, this, slot, es);
@@ -47,6 +50,7 @@ public class EndlessVenom extends Equipment {
 			sc.play(p, p);
 			pc.play(p, p);
 			data.addTask(new BukkitRunnable() {
+				@Override
 				public void run() {
 					inst.setCount(0);
 				}
@@ -55,16 +59,19 @@ public class EndlessVenom extends Equipment {
 		});
 		data.addTrigger(ID, bind, inst);
 		data.addTrigger(id, Trigger.BASIC_ATTACK, (pdata2, in) -> {
-			if (inst.getCount() == 0) return TriggerResult.keep();
+			if (inst.getCount() == 0)
+				return TriggerResult.keep();
 			BasicAttackEvent ev = (BasicAttackEvent) in;
 			FightInstance.applyStatus(ev.getTarget(), StatusType.POISON, data, poison, -1);
 			return TriggerResult.keep();
 		});
 	}
-
+	
 	@Override
 	public void setupItem() {
-		item = createItem(Material.GREEN_DYE,
-				"On cast, your basic attacks apply " + GlossaryTag.POISON.tag(this, poison, true) + ".");
+		item = createItem(
+				Material.GREEN_DYE,
+				"On cast, your basic attacks apply " + GlossaryTag.POISON.tag(this, poison, true) + "."
+		);
 	}
 }
