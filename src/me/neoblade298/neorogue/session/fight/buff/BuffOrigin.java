d18f947497ec70e9c6f14fb9058bbecb3e18ee2e
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2024-12-08 15:22:25 -0800

    Start reworking bufforigin

---
+++ src/me/neoblade298/neorogue/session/fight/buff/BuffOrigin.java
@@ -0,0 +1,11 @@
+import net.kyori.adventure.text.Component;
+
+public class BuffOrigin {
+    private Component display;
+    private String id;
+
+    public BuffOrigin(Component display, String id) {
+        this.display = display;
+        this.id = id;
+    }
+}
