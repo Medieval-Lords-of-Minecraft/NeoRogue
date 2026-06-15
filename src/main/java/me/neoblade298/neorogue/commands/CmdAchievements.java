f50a0818e6450da8786983592376eb3aabd67318
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-06-14 21:52:24 -0700

    Try to improve achievement messages

--- src/main/java/me/neoblade298/neorogue/commands/CmdAchievements.java
+++ src/main/java/me/neoblade298/neorogue/commands/CmdAchievements.java
@@ -5,6 +5,10 @@ import org.bukkit.entity.Player;
 
 import me.neoblade298.neocore.bukkit.commands.Subcommand;
 import me.neoblade298.neocore.shared.commands.SubcommandRunner;
+import me.neoblade298.neorogue.equipment.Equipment.EquipmentClass;
+import me.neoblade298.neorogue.player.PlayerData;
+import me.neoblade298.neorogue.player.PlayerManager;
+import me.neoblade298.neorogue.player.inventory.AchievementsInventory;
 import me.neoblade298.neorogue.player.inventory.AchievementsMenuInventory;
 
 public class CmdAchievements extends Subcommand {
@@ -15,6 +19,24 @@ public class CmdAchievements extends Subcommand {
 
 	@Override
 	public void run(CommandSender s, String[] args) {
-		new AchievementsMenuInventory((Player) s);
+		Player p = (Player) s;
+		if (args.length > 1) {
+			String scope = args[1].toLowerCase();
+			PlayerData pd = PlayerManager.getPlayerData(p.getUniqueId());
+			if (pd != null) {
+				EquipmentClass ec = null;
+				if (!scope.equals("global")) {
+					for (EquipmentClass cls : EquipmentClass.values()) {
+						if (cls.name().equalsIgnoreCase(scope)) {
+							ec = cls;
+							break;
+						}
+					}
+				}
+				new AchievementsInventory(p, pd, ec);
+				return;
+			}
+		}
+		new AchievementsMenuInventory(p);
 	}
 }
