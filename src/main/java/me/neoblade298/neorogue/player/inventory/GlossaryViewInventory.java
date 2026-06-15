dd80db710443ac7e5963f614a629dad4dd8507bf
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-06-14 17:34:09 -0700

    Fix another small glossary view bug

--- src/main/java/me/neoblade298/neorogue/player/inventory/GlossaryViewInventory.java
+++ src/main/java/me/neoblade298/neorogue/player/inventory/GlossaryViewInventory.java
@@ -44,7 +44,6 @@ public class GlossaryViewInventory extends GlossaryInventory {
 		}.runTask(NeoRogue.inst());
 	}
 
-    @Override
 	protected static int calculateSize(int numItems) {
 		int size = numItems + (9 - numItems % 9);
 		if (size < 9) size = 9;
