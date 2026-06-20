74ab2cff309fc44d0274909c899bd77820492845
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-06-20 14:32:01 -0700

    Add status scaling

--- src/main/java/me/neoblade298/neorogue/session/fight/mythicbukkit/MechanicStatus.java
+++ src/main/java/me/neoblade298/neorogue/session/fight/mythicbukkit/MechanicStatus.java
@@ -11,11 +11,13 @@ import io.lumine.mythic.api.skills.SkillResult;
 import io.lumine.mythic.api.skills.ThreadSafetyLevel;
 import io.lumine.mythic.bukkit.MythicBukkit;
 import io.lumine.mythic.core.mobs.ActiveMob;
+import me.neoblade298.neorogue.session.fight.FightData;
 import me.neoblade298.neorogue.session.fight.FightInstance;
+import me.neoblade298.neorogue.session.settings.NotorietySetting;
 
 public class MechanicStatus implements ITargetedEntitySkill {
 	protected final String id;
-	protected final boolean asParent;
+	protected final boolean asParent, scales;
 	protected final int stacks, ticks;
 
     @Override
@@ -28,6 +30,7 @@ public class MechanicStatus implements ITargetedEntitySkill {
         this.stacks = config.getInteger(new String[] { "a", "amount" }, 0);
         this.ticks = config.getInteger(new String[] { "t", "ticks" }, 0);
 		this.asParent = config.getBoolean(new String[] { "asParent", "ap" }, false);
+		this.scales = config.getBoolean(new String[] { "scales", "s" }, true);
 	}
 
 	@Override
@@ -38,7 +41,16 @@ public class MechanicStatus implements ITargetedEntitySkill {
 				return SkillResult.CONDITION_FAILED;
 			}
 			Entity ent = asParent ? am.getParent().get().getBukkitEntity() : data.getCaster().getEntity().getBukkitEntity();
-			FightInstance.applyStatus((LivingEntity) target.getBukkitEntity(), id, (LivingEntity) ent, stacks, ticks);
+			int finalStacks = stacks;
+			if (scales) {
+				double level = data.getCaster().getLevel();
+				FightData fd = asParent ? FightInstance.getFightData(am.getParent().get().getBukkitEntity())
+						: FightInstance.getFightData(data.getCaster().getEntity().getUniqueId());
+				boolean increaseDamageNotoriety = NotorietySetting.INCREASE_DAMAGE.isActive(fd.getInstance().getSession());
+				double mult = (1 + (level * 0.05)) * (increaseDamageNotoriety ? NotorietySetting.INCREASE_DAMAGE_MULTIPLIER : 1.0);
+				finalStacks = (int) (stacks * mult);
+			}
+			FightInstance.applyStatus((LivingEntity) target.getBukkitEntity(), id, (LivingEntity) ent, finalStacks, ticks);
 			return SkillResult.SUCCESS;
 		} catch (Exception e) {
 			e.printStackTrace();
