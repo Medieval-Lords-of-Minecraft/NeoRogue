65d1f5a2ef25a37aa539263272f9b1230895aa8d
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-06-21 15:06:50 -0700

    Small fixes

--- src/main/java/me/neoblade298/neorogue/equipment/abilities/Charge.java
+++ src/main/java/me/neoblade298/neorogue/equipment/abilities/Charge.java
@@ -1,6 +1,4 @@
 package me.neoblade298.neorogue.equipment.abilities;
-import me.neoblade298.neorogue.equipment.SessionEquipment;
-
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
@@ -8,14 +6,16 @@ import me.neoblade298.neorogue.DescUtil;
 import me.neoblade298.neorogue.equipment.Equipment;
 import me.neoblade298.neorogue.equipment.EquipmentInstance;
 import me.neoblade298.neorogue.equipment.EquipmentProperties;
+import me.neoblade298.neorogue.equipment.Power;
 import me.neoblade298.neorogue.equipment.Rarity;
+import me.neoblade298.neorogue.equipment.SessionEquipment;
 import me.neoblade298.neorogue.player.inventory.GlossaryTag;
 import me.neoblade298.neorogue.session.fight.PlayerFightData;
 import me.neoblade298.neorogue.session.fight.trigger.PriorityAction;
 import me.neoblade298.neorogue.session.fight.trigger.Trigger;
 import me.neoblade298.neorogue.session.fight.trigger.TriggerResult;
 
-public class Charge extends Equipment {
+public class Charge extends Equipment implements Power {
 	private static final String ID = "Charge";
 	private int shields;
 	
@@ -42,10 +42,17 @@ public class Charge extends Equipment {
 		data.addTrigger(id, Trigger.PLAYER_TICK, inst);
 		data.addTrigger(id, bind, new EquipmentInstance(data, sessionEq, slot, es, (pdata, in) -> {
 			if (inst.isActive()) return TriggerResult.remove();
-			inst.activate();
-			pdata.addSprintCost(1);
+			if (activatePower(data, slot, es)) {
+				inst.activate();
+				pdata.addSprintCost(1);
+			}
 			return TriggerResult.remove();
 		}));
+	}
+
+	@Override
+	public void onPowerActivated(PlayerFightData data, int slot, EquipSlot es) {
+		// Effect handled in ChargeInstance tick
 	}
 	
 	private class ChargeInstance extends PriorityAction {
