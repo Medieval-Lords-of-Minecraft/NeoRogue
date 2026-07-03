e8a4fa358d7371735cced72092230c2d44ffd08e
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-07-02 17:02:11 -0700

    Give boots of speed armor

--- src/main/java/me/neoblade298/neorogue/equipment/armor/BootsOfSpeed.java
+++ src/main/java/me/neoblade298/neorogue/equipment/armor/BootsOfSpeed.java
@@ -1,6 +1,4 @@
 package me.neoblade298.neorogue.equipment.armor;
-import me.neoblade298.neorogue.equipment.SessionEquipment;
-
 import java.util.UUID;
 
 import org.bukkit.Material;
@@ -8,6 +6,7 @@ import org.bukkit.Material;
 import me.neoblade298.neorogue.DescUtil;
 import me.neoblade298.neorogue.equipment.Equipment;
 import me.neoblade298.neorogue.equipment.Rarity;
+import me.neoblade298.neorogue.equipment.SessionEquipment;
 import me.neoblade298.neorogue.session.fight.DamageCategory;
 import me.neoblade298.neorogue.session.fight.PlayerFightData;
 import me.neoblade298.neorogue.session.fight.buff.Buff;
