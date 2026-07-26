a219d145ec1ec0d2435af6392059f895072723e9
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-07-25 20:33:26 -0700

    Leftover import

--- src/main/java/me/neoblade298/neorogue/equipment/abilities/Prayer.java
+++ src/main/java/me/neoblade298/neorogue/equipment/abilities/Prayer.java
@@ -1,6 +1,4 @@
 package me.neoblade298.neorogue.equipment.abilities;
-import me.neoblade298.neorogue.equipment.SessionEquipment;
-
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
@@ -11,6 +9,7 @@ import me.neoblade298.neorogue.equipment.Equipment;
 import me.neoblade298.neorogue.equipment.EquipmentProperties;
 import me.neoblade298.neorogue.equipment.Power;
 import me.neoblade298.neorogue.equipment.Rarity;
+import me.neoblade298.neorogue.equipment.SessionEquipment;
 import me.neoblade298.neorogue.player.inventory.GlossaryTag;
 import me.neoblade298.neorogue.session.fight.FightInstance;
 import me.neoblade298.neorogue.session.fight.PlayerFightData;
