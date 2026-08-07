9e09e0a13e57c301778837668d515441b4df6597
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-08-06 21:09:36 -0700

    import

--- src/main/java/me/neoblade298/neorogue/equipment/weapons/Maelstrom.java
+++ src/main/java/me/neoblade298/neorogue/equipment/weapons/Maelstrom.java
@@ -1,6 +1,4 @@
 package me.neoblade298.neorogue.equipment.weapons;
-import me.neoblade298.neorogue.equipment.SessionEquipment;
-
 import java.util.HashSet;
 import java.util.LinkedList;
 
@@ -23,6 +21,7 @@ import me.neoblade298.neorogue.equipment.Equipment;
 import me.neoblade298.neorogue.equipment.EquipmentProperties;
 import me.neoblade298.neorogue.equipment.EquipmentProperties.PropertyType;
 import me.neoblade298.neorogue.equipment.Rarity;
+import me.neoblade298.neorogue.equipment.SessionEquipment;
 import me.neoblade298.neorogue.equipment.mechanics.Barrier;
 import me.neoblade298.neorogue.equipment.mechanics.Projectile;
 import me.neoblade298.neorogue.equipment.mechanics.ProjectileGroup;
